f3fd451ce3a51fbb8d7e97a0ea76d7e5036f88fa
Justin Barnikow <justinsbarnikow@gmail.com>
2022-01-30 10:03:16 -0500

    navbar color added

--- src/HomePageSections/NavBar.js
+++ src/HomePageSections/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NavBar() {
   return (
     <section className='navbar'>
-        <span className='quick_menu'>Menu:</span>
+        <span className='quick_menu'>menu:</span>
         <a href='#about_section'>about</a>
         <a href='#projects_section'>projects</a>
         <a href='#contact_section'>contact</a>
